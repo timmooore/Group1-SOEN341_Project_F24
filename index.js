fbc23148b8e16b6dd3c08354878e344c14e69c52
Greg Sacciadis <greg.sacciadis@gmail.com>
2024-09-22 15:33:45 -0400

    Set up register route with passport

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const bcrypt = require('bcrypt');
 const session = require('express-session');
-const app = express();
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const app = express(); 
 
 //Models
 const User = require('./models/user');
@@ -27,22 +28,17 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')) 
 
 //This helps with parsing URL data - good to include for our forms
-//Parses the request body
-app.use(express.urlencoded({ extended: true }));
 
-//Sessions provide statefullness & will help keep users logged in / logged out
-app.use(session({secret : 'soen341'}));
-//Remember to npm install method-override for this
-//This helps with changing/naming the HTTP Requests
-app.use(methodOverride('_method'));
+//Fix this session with sessionConfig - TO DO LATER**
+app.use(session({secret : 'soen341'})); //Sessions provide statefullness & will help keep users logged in / logged out
+app.use(express.urlencoded({ extended: true })); //Helps with parsing the request body
+app.use(methodOverride('_method')); //This helps with changing/naming the HTTP Requests
 
-//MIDDLEWARE
-const requireLogin = (req, res, next) => {
-    if (!req.session.user_id) {
-        return res.redirect('/login')
-    }
-    next();
-}
+app.use(passport.initialize());
+app.use(passport.session()); //Be sure to 'use' this after we use 'session'
+passport.use(new LocalStrategy(User.authenticate())); //Telling passport to use the passport-given authentication method for our User model
+passport.serializeUser(User.serializeUser()); //How to store a user in the session i.e. log them in & keep them logged in
+passport.deserializeUser(User.deserializeUser()); //How to remove a user from a session i.e. log them out
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -54,35 +50,26 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', async (req, res) => {
-    const { password, username } = req.body;
-    const user = new User({ username, password })
-    await user.save();
-    req.session.user_id = user._id;
-    res.redirect('/')
+    const { username, password, user_type } = req.body;
+    const user = new User({ username, user_type });
+    const registeredUser = await User.register(user, password);
+    console.log(registeredUser);
+    res.redirect('/secret');
 })
 
 app.get('/login', (req, res) => {
     res.render('login')
 })
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const foundUser = await User.findAndValidate(username, password);
-    if (foundUser) {
-        req.session.user_id = foundUser._id;
-        res.redirect('/secret');
-    }
-    else {
-        res.redirect('/login')
-    }
+app.post('/login', (req, res) => {
+    res.redirect('/secret');
 })
 
 app.post('/logout', (req, res) => {
-    req.session.user_id = null;
-    //req.session.destroy();
+    //Changing this to implement passport
     res.redirect('/login');
 })
 
-app.get('/secret', requireLogin, (req, res) => {
+app.get('/secret', (req, res) => {
     res.render('secret')
 })
 
--- models/user.js
+++ models/user.js
@@ -1,27 +1,18 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
-const userSchema = new mongoose.Schema({
-    username: {
+const UserSchema = new Schema({
+    user_type: {
         type: String,
-        required: [true, 'Username cannot be blank']
-    },
-    password: {
-        type: String,
-        required: [true, 'Password cannot be blank']
+        enum: ['student', 'instructor'], //Only allows users to be "students" or "instructors"
+        required: true
     }
-})
+});
 
-userSchema.statics.findAndValidate = async function (username, password) {
-    const foundUser = await this.findOne({ username });
-    const isValid = await bcrypt.compare(password, foundUser.password);
-    return isValid ? foundUser : false;
-}
+/* Note: 
+No need to specify username or password, since passport does it for us.
+Passport also makes sure the usernames are unique & offers several methods onto our Schema */
+UserSchema.plugin(passportLocalMongoose);
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 12);
-    next();
-})
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
