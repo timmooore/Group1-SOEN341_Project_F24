2354bd720256f9e214364d8e349f760d40967b3a
Greg Sacciadis <greg.sacciadis@gmail.com>
2024-09-24 01:05:58 -0400

    Finished login, register, and logout logic

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const flash = require('connect-flash'); //Will use this later, but we haven't set much up yet 
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const app = express(); 
@@ -35,7 +36,7 @@ app.use(express.urlencoded({ extended: true })); //Helps with parsing the reques
 app.use(methodOverride('_method')); //This helps with changing/naming the HTTP Requests
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
+    secret: 'soen341',
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -47,13 +48,24 @@ const sessionConfig = {
 
 
 app.use(session(sessionConfig));
-//Flash to be installed, later
+//app.use(flash()); //Need to set this up
 
 app.use(passport.initialize());
 app.use(passport.session()); //Be sure to 'use' this after we use 'session'
 passport.use(new LocalStrategy(User.authenticate())); //Telling passport to use the passport-given authentication method for our User model
+
 passport.serializeUser(User.serializeUser()); //How to store a user in the session i.e. log them in & keep them logged in
 passport.deserializeUser(User.deserializeUser()); //How to remove a user from a session i.e. log them out
+
+//Middleware that'll execute on every request to the server
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user; //Has user info in the req session - Important !
+    /* Setting this up, later, once we're more ahead.
+    res.locals.success = req.flash('success'); //Flashes a message if the req succeeds (The specific messages are set up in the routes)
+    res.locals.error = req.flash('error'); //Flashes a message if the req fails (The specific messages are set up in the routes)
+    */ 
+    next();
+})
 
 //ROUTES
 //Note: Will move these into separate route folders, eventually*
@@ -67,32 +79,38 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', async (req, res) => {
-    const { username, password, user_type } = req.body;
-    const user = new User({ username, user_type });
-    const registeredUser = await User.register(user, password);
-    console.log(registeredUser);
-    res.redirect('/secret');
+    try {
+        const { username, password, user_type } = req.body;
+        const user = new User({ username, user_type });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if(err) return next(err);
+            res.redirect('/secret');
+        })
+    } catch(e) {
+        res.redirect('register');
+    }
 })
 
 app.get('/login', (req, res) => {
     res.render('login')
 })
-app.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), (req, res) => {
+app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
+    //Login fix
     res.redirect('/secret');
 })
 
 //Passport logout requires a callback function, so it has a bit more code
-app.get('/logout', (req, res, next) => {
+app.post('/logout', (req, res, next) => {
     req.logout(function (err) {
         if (err) {
             return next(err);
         }
-        //Implement Flash here, later*
         res.redirect('/login');
     });
 });
 
-app.get('/secret', (req, res) => {
+app.get('/secret', isLoggedIn, (req, res) => {
     res.render('secret');
 })
 
--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) { 
         //Add flash here, later => *Advise user they need to be logged in to proceed
-        console.log("Not authenticated");
+        console.log("Not authenticated"); //For testing purposes
         return res.redirect('/login');
     }
     next();
